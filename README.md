7d9b8d0d2979ae964d1e0762c0cc9e2b62724dcf
refactor <mjollnir.ray@gmail.com>
2012-06-15 18:56:18 +0800

    use img_tiler to copyout tiles

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ geo-image shredder, cut out tiles along space-cell, like gdal2tiles
 
 application:start(compiler), application:start(syntax_tools), application:start(lager).
 lager:set_loglevel(lager_console_backend, debug).
-{ok,Pid} = img_scanner:start_link(global_mercator, "../tz/tzh.tif", {"10.0.72.1", 8087}).
+{ok,Pid} = img_scanner:start_link(global_mercator, "../tz/tzh.tif", {"127.0.0.1", 8087}).
 
--- src/img_scanner.erl
+++ src/img_scanner.erl
@@ -20,9 +20,9 @@
 -record(state, {refs = [],
                 riakclient     :: pid(),
                 counter = 0,
+                img_tiler,
                 map_profile,    % tile map profile, 
                                 % such as global-geodetic or global-mercator module
-                img_filename    :: string(),
                 tile_size = 256 :: integer()}).
 
 -record(tile_position, {current_tile_x :: integer(), 
@@ -39,15 +39,12 @@ complete(Pid, Ref, Result) ->
 init({ProfileMod, ImgFileName, {RiakIp, RiakPort} = _RiakClientConfig}) ->
     {ok, RiakClientSocketPid} = riakc_pb_socket:start_link(RiakIp, RiakPort),
 
-    self() ! start,
+    self() ! {start, ImgFileName},
     {ok, copyouting, #state{riakclient  = RiakClientSocketPid,
-                            map_profile = ProfileMod, 
-                            img_filename= ImgFileName}}.
+                            map_profile = ProfileMod}}.
 
-copyouting({continue, {Img, RasterInfo, TileX, TileY, TileZoom}, Continuation}, State=#state{refs=Refs}) ->
-    {ok, TileRawdata} = global_grid:copyout_tile_for(State#state.map_profile, 
-                                                     TileX, TileY, TileZoom, 
-                                                     Img, RasterInfo),
+copyouting({continue, {TileX, TileY, TileZoom}, Continuation}, State=#state{refs=Refs, img_tiler=ImgTiler}) ->
+    {ok, TileRawdata} = ImgTiler:copyout_tile_for(TileX, TileY, TileZoom),
     Ref = make_ref(),
     tile_builder:start_link({self(), Ref}, {TileRawdata, {TileX, TileY, TileZoom}}, State#state.riakclient),
     gen_fsm:send_event(self(), Continuation()),
@@ -80,9 +77,12 @@ handle_event(_Event, StateName, StateData) ->
 handle_sync_event(_Event, _From, StateName, StateData) ->
     {next_state, StateName, StateData}.
 
-handle_info(start, StateName, StateData = #state{map_profile=ProfileMod, img_filename=ImgFileName}) ->
-    gen_fsm:send_event(self(), img_scanner:scan_img(ProfileMod, ImgFileName)),
-    {next_state, StateName, StateData}.
+handle_info({start, ImgFileName}, StateName, StateData = #state{map_profile=ProfileMod}) ->
+    {ok, Img, RasterInfo} = gdal_nif:create_warped_vrt(ImgFileName, 
+                                                       ProfileMod:epsg_code()),
+    ImgTiler = img_tiler:new(ProfileMod, Img, RasterInfo),
+    gen_fsm:send_event(self(), scan_img(ImgTiler)),
+    {next_state, StateName, StateData#state{img_tiler=ImgTiler}}.
 
 terminate(_Reason, _StateName, _StateData) ->
     ok.
@@ -91,50 +91,42 @@ code_change(_OldVsn, State, Data, _Extra) ->
     {ok, State, Data}.
 
 
-scan_img(ProfileMod, ImgFileName) ->
-    {ok, Img, RasterInfo} = gdal_nif:create_warped_vrt(ImgFileName, 
-                                                       ProfileMod:epsg_code()),
-    {TileZoom, TileEnclosure} = global_grid:calc_tiles_enclosure(ProfileMod, RasterInfo),
+scan_img(ImgTiler) ->
+    {TileZoom, TileEnclosure} = ImgTiler:calc_tiles_enclosure(),
     {StartTileX, StartTileY, _, _} = TileEnclosure,
-    {continue, {Img, RasterInfo, StartTileX, StartTileY, TileZoom},
+    {continue, {StartTileX, StartTileY, TileZoom},
                fun() ->
-                    scan_img(ProfileMod, 
-                             Img, 
-                             RasterInfo, 
+                    scan_img(ImgTiler,
                              #tile_position{current_tile_x = StartTileX, 
                                             current_tile_y = StartTileY, 
                                             tile_zoom      = TileZoom,
                                             tile_enclosure = TileEnclosure})
                end}.
 
-scan_img(_ProfileMod, _Img, _RasterInfo, #tile_position{current_tile_x = MaxX,
-                                                        current_tile_y = MaxY,
-                                                        tile_zoom      = _TileZoom,
-                                                        tile_enclosure = {_,_, MaxX,MaxY}}) ->
+scan_img(_ImgTiler, #tile_position{current_tile_x = MaxX,
+                                   current_tile_y = MaxY,
+                                   tile_zoom      = _TileZoom,
+                                   tile_enclosure = {_,_, MaxX,MaxY}}) ->
     done;
-scan_img(ProfileMod, Img, RasterInfo, #tile_position{current_tile_x = MaxX,
-                                                     current_tile_y = Y,
-                                                     tile_zoom      = TileZoom,
-                                                     tile_enclosure ={MinX,MinY,MaxX,MaxY}}) ->
-    {continue, {Img, RasterInfo, MinX, Y + 1, TileZoom},
+scan_img(ImgTiler, #tile_position{current_tile_x = MaxX,
+                                  current_tile_y = Y,
+                                  tile_zoom      = TileZoom,
+                                  tile_enclosure ={MinX,MinY,MaxX,MaxY}}) ->
+    {continue, {MinX, Y + 1, TileZoom},
                 fun() ->
-                    scan_img(ProfileMod, 
-                             Img, 
-                             RasterInfo, 
+                    scan_img(ImgTiler, 
                              #tile_position{current_tile_x = MinX,
                                             current_tile_y = Y + 1,
                                             tile_zoom      = TileZoom,
                                             tile_enclosure = {MinX,MinY,MaxX,MaxY}})
                 end};
-scan_img(ProfileMod, Img, RasterInfo, #tile_position{current_tile_x = X,
-                                                     current_tile_y = Y,
-                                                     tile_zoom      = TileZoom,
-                                                     tile_enclosure={MinX,MinY,MaxX,MaxY}}) ->
-    {continue, {Img, RasterInfo, X + 1, Y, TileZoom},
+scan_img(ImgTiler, #tile_position{current_tile_x = X,
+                                  current_tile_y = Y,
+                                  tile_zoom      = TileZoom,
+                                  tile_enclosure={MinX,MinY,MaxX,MaxY}}) ->
+    {continue, {X + 1, Y, TileZoom},
                 fun() ->
-                    scan_img(ProfileMod, 
-                             Img, 
-                             RasterInfo, 
+                    scan_img(ImgTiler, 
                              #tile_position{current_tile_x = X + 1,
                                             current_tile_y = Y,
                                             tile_zoom      = TileZoom,
